655a3e109306b4512ac53447508607120f2940fb
AMS2504 <arjunmsaxenaofficiak@gmail.com>
2026-07-14 00:52:47 +0530

    bug fixes

--- repositories/portfolio_repository.py
+++ repositories/portfolio_repository.py
@@ -18,7 +18,7 @@ def get_user_portfolio(user_id: int) -> Portfolio | None:
     )
     for pos_row in positions_rows:
         position = Position(
-            symbol=pos_row["symbol"],
+            symbol=pos_row["stock_name"],
             quantity=pos_row["quantity"],
             avg_buy_price=pos_row["avg_buy_price"],
         )
@@ -34,15 +34,15 @@ def save_user_portfolio(user_id: int, portfolio: Portfolio):
     )
     for position in portfolio.positions.values():
         db_manager.execute(
-            "INSERT OR REPLACE INTO holdings (user_id, symbol, quantity, avg_buy_price) VALUES (?, ?, ?, ?)",
+            "INSERT OR REPLACE INTO holdings (user_id, stock_name, quantity, avg_buy_price) VALUES (?, ?, ?, ?)",
             (user_id, position.symbol, position.quantity, position.avg_buy_price),
         )
 
 def record_user_transaction(user_id: int, transaction: Transaction):
     db_manager = DatabaseManager("database\PaperTrader.db")  # Initialize the database manager
     db_manager.execute(
-        "INSERT INTO transactions (user_id, symbol, side, quantity, price, timestamp) VALUES (?, ?, ?, ?, ?, ?)",
-        (user_id, transaction.symbol, transaction.side, transaction.quantity, transaction.price, transaction.timestamp),
+        "INSERT INTO transactions (user_id, stock_name, transaction_type, quantity, price, timestamp) VALUES (?, ?, ?, ?, ?, ?)",
+        (user_id, transaction.symbol, transaction.side.upper(), transaction.quantity, transaction.price, transaction.timestamp),
     )
     save_user_portfolio(user_id, get_user_portfolio(user_id))  # Update the portfolio after recording the transaction
 
@@ -54,11 +54,11 @@ def get_user_transaction_history(user_id: int) -> list[Transaction]:
     transactions = []
     for row in transactions_rows:
         transaction = Transaction(
-            symbol=row["symbol"],
-            side=row["side"],
+            symbol=row["stock_name"],
+            side=row["transaction_type"].lower(),
             quantity=row["quantity"],
             price=row["price"],
-            timestamp=row["timestamp"]
+            timestamp=row["timestamp"],
         )
         transactions.append(transaction)
     return transactions
